c30978bd0eb0f3be278e94522cc8a48885785b91
Ramkishor H <ramhosamane@gmail.com>
2020-12-02 21:45:37 +0530

    implemented element options

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem,copyArrayItem} from '@angular/cdk/drag-drop';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-builder',
@@ -8,9 +9,9 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem,copyArrayItem} from '@an
 })
 export class BuilderComponent implements OnInit {
   public input_elements:any = [
-    { type:'text',name:'',id:'',class:'',show:true},
-    { type:'email',name:'',id:'',class:'' ,show:true},
-    { type:'password',name:'',id:'',class:'' ,show:true}
+    { type:'text',name:'text',placeholder:'',id:'',class:'',show:true},
+    { type:'email',name:'email',placeholder:'',id:'',class:'' ,show:true},
+    { type:'password',name:'password',placeholder:'',id:'',class:'' ,show:true}
   ];
   /*
 
@@ -18,10 +19,59 @@ export class BuilderComponent implements OnInit {
   public design_elements :any  = [
     
   ];
+  public selected_element:any = null;
+  public selected_option_name = "";
+  public selected_option_placeholder = "";
+  public selected_option_id = "";
+  public selected_option_class = "";
 
-  constructor() { }
+  public is_show_element_options : boolean = false;
+  
+  
+  constructor() { 
+  }
+
+
+  select_options(element:any){
+    this.is_show_element_options = true;
+    this.selected_element = element;
+    this.selected_option_name = element.name;
+    this.selected_option_id = element.id;
+    this.selected_option_class = element.class;
+    this.selected_option_placeholder = element.placeholder;
+
+  }
+
+  onKeyUpName(){
+    this.selected_element.name = this.selected_option_name;//value;
+  }
+
+  onKeyUpId(){
+    this.selected_element.id = this.selected_option_id;
+  }
+  onKeyUpClass(){
+    this.selected_element.class = this.selected_option_class;
+  }
+  onKeyUpPlaceholder(){
+    this.selected_element.placeholder = this.selected_option_placeholder;
+  }
+
+
+  deleteElement(){
+    var index = this.design_elements.indexOf(this.selected_element);
+    this.design_elements.splice(index,1);
+    this.clearSelectedElement();      
+  }
+
+  clearSelectedElement(){
+    this.selected_element = null;
+    this.selected_option_placeholder = "";
+    this.selected_option_id = "";
+    this.selected_option_name = "";
+    this.selected_option_class = "";
+    this.is_show_element_options = false;    
+  }
   drop(event: CdkDragDrop<object[]>) {
-    console.log(event)
     if (event.previousContainer === event.container) {      
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
